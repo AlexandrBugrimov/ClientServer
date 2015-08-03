3f1c1a264cdcdf171dcec941d35ad5eb418e3de8
BugrimovA <b_a82@mail.ru>
2015-08-03 21:11:17 +0600

    Преобразование принятой от клиента строки

--- src/ru/bugrimov/client_server/Server.java
+++ src/ru/bugrimov/client_server/Server.java
@@ -15,9 +15,9 @@ import java.util.StringTokenizer;
 
 public class Server {
     protected static ServerSocket server;
-    private static Socket client;
     private static PrintWriter writer;
     private static BufferedReader reader;
+    private static Socket client;
 
     public static void main(String[] args) {
         try {
@@ -26,19 +26,21 @@ public class Server {
 
             final String HOST = configuration.getLocalhost();
             final int PORT = configuration.getPort();
-
             server = new ServerSocket(PORT);
-            System.out.println(" * Сервер \"" + HOST + ": " + PORT + "\" запущен. *");
-            client = server.accept();
+
+            System.out.println(" Сервер <" + HOST + ": " + PORT + "> - запущен.");
+
 
             for (;;) {
+                client = server.accept();
                 writer = new PrintWriter(client.getOutputStream());
                 reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
-
                 String str = reader.readLine();
-                System.out.println(" Запрос клиента -> " + str);
-                writer.write(reversString(str) + "\n");
-                writer.flush();
+                if (str != null) {
+                    System.out.println(" Запрос клиента -> " + str);
+                    writer.write(reversString(str) + "\n");
+                    writer.flush();
+                }
             }
         } catch (IOException | NullPointerException e) {
             System.err.println(" Ошибка: " + e.getMessage());
@@ -46,8 +48,8 @@ public class Server {
             if (server != null || client != null || writer != null || reader != null) {
                 try {
                     server.close();
-                    client.close();
                     writer.close();
+                    client.close();
                     reader.close();
                 } catch (IOException ignored) {
                 }
